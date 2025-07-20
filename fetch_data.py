35cf53aeba9e616c7d6e4320be15ff0f9b07b80c
georgiygusev <georgiy.gus@yandex.ru>
2025-07-20 20:30:32 +0300

    set standard filed ts

--- fetch_data.py
+++ fetch_data.py
@@ -9,11 +9,18 @@ GET_ADS_STATS_URL = "https://advert-api.wildberries.ru/adv/v2/fullstats"
 GET_ADS_INFO_URL = "https://advert-api.wildberries.ru/adv/v1/promotion/adverts"
 
 
-async def fetch_data(api_token: str, campaign_ids: list) -> list:
+async def fetch_data(api_token: str, campaigns: list) -> list:
     headers = {"Authorization": api_token}
     result = []
+
+    # TODO: пока что норм, но в будущем переделать. Тк сейчас мы обрабатываем только те компании, которые активны на момент получения данных. Но мы в системе обрабатываем данные за прошлые сутки, а компания вчера могла быть активной, а сегодня до получения данных ее остановили, и она не учитывается. Надо как то шарить данные по активным кампаниям за вчера
+    campaigns_with_correct_status = []
+    for i in campaigns:
+        if int(i['status']) == 9:
+            campaigns_with_correct_status.extend([i['advertId'] for i in i['advert_list']])
+
     async with aiohttp.ClientSession(headers=headers) as session:
-        for batch in chunked(campaign_ids, 50):
+        for batch in chunked(campaigns_with_correct_status, 50):
             async with session.post(GET_ADS_INFO_URL, json=batch) as response:
                 data = await response.json()
                 response.raise_for_status()
